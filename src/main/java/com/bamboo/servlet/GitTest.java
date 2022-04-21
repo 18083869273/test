143f9dcdd9786f13cd84d0c92fb4c3e81710ed7b
bamboo <3043400744@qq.com>
2022-04-21 23:16:09 +0800

    lxy four commit

--- src/main/java/com/bamboo/servlet/GitTest.java
+++ src/main/java/com/bamboo/servlet/GitTest.java
@@ -8,7 +8,6 @@ package com.bamboo.servlet;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
-        System.out.println("假如生活欺骗了你");
-        System.out.println("hello zhaosizhu!");
+        System.out.println("假如生活欺骗了你 不要悲伤 不要难过 忧郁的日子里须镇静 ");
     }
 }
