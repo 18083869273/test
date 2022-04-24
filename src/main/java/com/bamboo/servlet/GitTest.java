979b9c36811ace3fb95e8e6f1df9f4435ca4abe7
bamboo <3043400744@qq.com>
2022-04-24 12:51:11 +0800

    hot-fix commit two

--- src/main/java/com/bamboo/servlet/GitTest.java
+++ src/main/java/com/bamboo/servlet/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("假如生活欺骗了你 不要悲伤 不要难过 忧郁的日子里须镇静 ");
         System.out.println("hello Git 0205");
+        System.out.println("hello Bamboo");
     }
 }
